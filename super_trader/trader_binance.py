ac23d5dee030e8609dd6d552a7347c3827857e74
uihyunglee <uihyunglee.tech@gmail.com>
2023-11-28 17:07:27 +0900

    Create trader_binance.py

---
+++ super_trader/trader_binance.py
@@ -0,0 +1,13 @@
+from datetime import datetime as dt
+from datetime import timedelta as td
+import time
+
+import ccxt
+import requests
+import os
+
+from super_trader import SuperTrader
+
+
+class BinanceTrader(SuperTrader):
+    pass
