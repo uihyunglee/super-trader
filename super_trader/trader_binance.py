e3bcf9d0faa5b3b4f70e85fb863d75161f2e6725
uihyunglee <uihyunglee.tech@gmail.com>
2023-12-19 23:54:34 +0900

    FEAT: Add read_api_key

--- super_trader/trader_binance.py
+++ super_trader/trader_binance.py
@@ -1,13 +1,30 @@
+import json
+import os
+import time
 from datetime import datetime as dt
 from datetime import timedelta as td
-import time
 
 import ccxt
 import requests
-import os
 
 from super_trader import SuperTrader
 
 
 class BinanceTrader(SuperTrader):
-    pass
+
+    def read_api_key(self):
+        config_path = os.path.join(os.getcwd(), "config.json")
+        if os.path.exists(config_path):
+            with open('config.json', 'r') as json_file:
+                _config = json.load(json_file)
+            if 'binance' in _config:
+                binance_info = _config['binance']
+                required_keys = ["api_key", "secret"]
+                if all(key in binance_info for key in required_keys):
+                    return binance_info
+                else:
+                    raise ValueError("binance in config.json must contain 'api_key', and 'secret' key.")
+            else:
+                raise ValueError("config.json must contain 'binance' key.")
+        else:
+            raise FileNotFoundError("config.json must exist in the path. ")
